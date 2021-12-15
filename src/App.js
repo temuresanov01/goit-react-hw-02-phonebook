3975c336682784045e1c73844294f6ef3169b567
Temur Esanov <temuresanov01@gmail.com>
2021-12-15 09:49:07 +0100

    add css

--- src/App.js
+++ src/App.js
@@ -1,31 +1,6 @@
-// import logo from "./logo.svg";
-// import "./App.css";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { Component } from 'react';
 import { v4 as uuid } from 'uuid';
+import './App.css';
 import Section from './components/Section/Section';
 import ContactList from './components/ContactList/ContactList';
 import ContactForm from './components/ContactForm/ContactForm';
